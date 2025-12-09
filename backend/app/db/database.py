dc9724755a9769cb8fb5b763aee82b560d571604
Yenni009 <yennimedrano02@gmail.com>
2025-12-09 11:46:02 -0500

    Subiendo salvacion

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,18 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SOLO usar pymysql en Railway
-SQLALCHEMY_DATABASE_URL = (
-    "mysql+pymysql://root:XFpqfuTgzbyjpZbIqiZnvDJmYOnuvUjH"
-    "@centerbeam.proxy.rlwy.net:24823/railway"
-)
+# Cambiar este valor a True si quieres usar Railway, False si quieres local
+USE_RAILWAY = True
+
+if USE_RAILWAY:
+    SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:XFpqfuTgzbyjpZbIqiZnvDJmYOnuvUjH@centerbeam.proxy.rlwy.net:24823/railway"
+else:
+    SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/todo_db"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -8,16 +8,15 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# CORS limpio y único
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://toddollistc.netlify.app"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://toddollistc.netlify.app"
+],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
